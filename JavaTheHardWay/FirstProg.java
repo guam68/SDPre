15701cd9734000e814b318fe6eff52d1e3518670
guam68 <guam_68@hotmail.com>
2019-06-21 23:39:38 -0500

    Exercises 1-4

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class FirstProg {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println( "I, John Doe, am determined to learn how to code." );
+        System.out.println( "Today's date is " + date.toString() );
+    }
+}
