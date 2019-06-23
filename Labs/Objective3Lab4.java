ec9cc402b6fa28881b3e4b28059e65629d37388d
guam68 <guam_68@hotmail.com>
2019-06-23 16:37:58 -0500

    pre obj 3 lab 1-5, jthw ex 7-9

---
+++ Labs/Objective3Lab4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective3Lab4 {
+    public static void main(String[] args) {
+        String firstName;
+        String lastName;
+        int age;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        firstName = input.next();
+        System.out.println("Enter last name: ");
+        lastName = input.next();
+        System.out.println("Enter your age: ");
+        age = input.nextInt();
+
+        System.out.println("First Name: " + firstName + ", Last Name: " + lastName + ", Age: " + age);
+
+        input.close();
+    }
+}
